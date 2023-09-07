0b0e203c9d1e8af003149faf9ff15c48c9b440a1
Ari Meron <ari@involve.ai>
2023-09-07 12:44:32 -0700

    Changed file name

--- blobTester.java
+++ blobTester.java
@@ -1,12 +1,12 @@
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public class blobTester {
-    public static void main(String[] args) {
-        StringBuilder ex = new StringBuilder("tottenham");
-        try {
-            System.out.println(blobAndIndex.generateSha1(ex));
-        } catch (NoSuchAlgorithmException e){
-            System.out.println(e);
-        }
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        Git.init();
+        Git.blob("hello.txt");
+        Git.blob("hi.txt");
+        Git.blob("h.txt");
     }
 }
