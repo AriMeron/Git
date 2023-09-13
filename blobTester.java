34ca50940e94eb405ee41d0f3f4bec412a3ca262
Allie <allie.hunnius@gmail.com>
2023-09-13 14:11:44 -0700

    created the needed files for the tester

--- blobTester.java
+++ blobTester.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
@@ -5,8 +6,11 @@ public class blobTester {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
         Git.init();
+        File file = new File ("hello.txt");
         Git.blob("hello.txt");
+        File file2 = new File ("hi.txt");
         Git.blob("hi.txt");
+        File file3 = new File ("h.txt");
         Git.blob("h.txt");
 
         Git.remove("hello.txt");
