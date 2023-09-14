4d2a18eaa0ca6e08e2a26123ba98ec79213622cc
Allie <allie.hunnius@gmail.com>
2023-09-14 16:27:46 -0700

    delete

--- Tree.java
+++ Tree.java
@@ -9,11 +9,14 @@ public class Tree {
     private File file;
     private ArrayList trees;
     private ArrayList names;
+    private String treeFileName;
+
     public Tree (String fileName)
     {
         file = new File (fileName);
         names = new ArrayList <String> ();
         trees = new ArrayList <String> ();
+        treeFileName = fileName;
     }
 
     public void add (String typeOfFile, String shaOfFile, String optionalFileName) throws IOException
@@ -38,25 +41,31 @@ public class Tree {
         fw.close();
     }
 
-    public void remove (String string)
+    public void remove (String string) throws Throwable
     {
-        Boolean isFile = true;
+        int index;
+        String theFile = fileToString (treeFileName);
+        index = theFile.indexOf (string);
+        int length;
+        String endString;
+        
         if (string.contains (".txt"))
         {
-            isFile = false;
-            
-            
-            StringBuilder sb = new StringBuilder ();
-
+            length = 50;
+            endString = string.substring (0, index - length) + string.substring (index + string.length() + 1);
+            stringToFile (endString, treeFileName);
         }
         else
         {
+            length = 7;
+            endString = string.substring (0, index - length) + string.substring (index + string.length() + 1);
+            stringToFile (endString, treeFileName);
 
         }
     }
 
 
-     public  void stringToFile (String string, String fileName) throws IOException
+     public void stringToFile (String string, String fileName) throws IOException
         {
             // attach a file to FileWriter
             File file = new File ("objects/" + string);
@@ -79,4 +88,35 @@ public class Tree {
             fw.close();
             fr.close();
         }
+
+        public String fileToString (String fileName) throws Throwable
+        {
+            String endResult = "";
+            //File file = new File ("output.txt");
+            char ch;
+ 
+             // check if File exists or not
+            FileReader fr;
+            try
+            {
+                fr = new FileReader(fileName);
+                while(fr.ready())
+                {
+                    ch = (char) fr.read();
+                    endResult += ch;
+                }
+ 
+                fr.close();
+            } catch (Error | IOException e)
+            {
+                
+                throw e;
+            }
+            return endResult;
+        }
+
+
+
+
+
 }
