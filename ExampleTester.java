c1513ebf50aa0c7b87f5e5f1df2a275cd4edd9cb
Allie <allie.hunnius@gmail.com>
2023-09-15 11:43:51 -0700

    literally just added a comment about what i'm confused about (checking directory)

--- ExampleTester.java
+++ ExampleTester.java
@@ -75,6 +75,7 @@ public class ExampleTester {
         tree.remove("bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
         tree.writeToObjects();
         assertTrue (tree.getName().equals (sha));
+        //how do i check that its in the directory "objects"
 
 
     }
