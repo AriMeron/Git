5cb36f5dd0ca48d46a185cb4aa14c16924201cd4
Ari Meron <ari@involve.ai>
2023-09-22 14:08:47 -0700

    Fixed Merge Conflict

---
+++ blobTester.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class blobTester {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        Git.init();
+        Git.blob("hello.txt");
+        Git.blob("hi.txt");
+        Git.blob("h.txt");
+
+        Git.remove("hi.txt");
+    }
+}
