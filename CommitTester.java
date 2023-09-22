40f16da6def0351c753c04baa50424b8f4112385
wyattlake <lakewyatt@gmail.com>
2023-09-21 21:08:34 -0700

    Created Commit and CommitTester, Added Labels to GitTester

---
+++ CommitTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CommitTester {
+
+}
--- GitTester.java
+++ GitTester.java
@@ -3,16 +3,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GitTester {
 
     @Test
+    @DisplayName("Verify sha1 hashing works")
     void testGenerateSha1() throws Exception {
         assertEquals(Git.generateSha1("Hello world."), "e44f3364019d18a151cab7072b5a40bb5b3e274f");
     }
 
     @Test
+    @DisplayName("Verify init creates index file and objects folder")
     void testInit() throws IOException {
         Git.init();
 
@@ -24,6 +27,7 @@ public class GitTester {
     }
 
     @Test
+    @DisplayName("Verify blob creates blobs and adds them to index")
     void testBlob() throws Exception {
         // Because blob is designed to create the blob and update the index, this test
         // tests both of those functionalities
@@ -47,6 +51,7 @@ public class GitTester {
     }
 
     @Test
+    @DisplayName("Verify remove correctly updates index")
     void testRemove() throws Exception {
         Git.init();
 
@@ -65,10 +70,16 @@ public class GitTester {
         // Confirm testFile.txt has been removed from index
         assertEquals(Util.readFile("index"), "testFile2.txt : dd6fdaba4cff3db9692d2a86b39a331ad92c0667");
 
+        // Confirm the blob file still exists
+        assertTrue(Util.exists("objects/26d82f1931cbdbd83c2a6871b2cecd5cbcc8c26b"));
+
         Git.remove("testFile2.txt");
 
         // Confirm testFile2.txt has been removed from index
         assertEquals(Util.readFile("index"), "");
+
+        // Confirm the blob file still exists
+        assertTrue(Util.exists("objects/dd6fdaba4cff3db9692d2a86b39a331ad92c0667"));
 
         Util.deleteDirectory("objects");
         Util.deleteFile("index");
