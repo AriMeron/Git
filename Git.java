39aa146c1384c740239a1b1fca83ff70fa5f1e13
Ari Meron <ari@involve.ai>
2023-09-26 23:49:29 -0700

    Added addDirectory() and fixed tests

--- Git.java
+++ Git.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,9 +14,13 @@ import java.security.NoSuchAlgorithmException;
 
 public class Git {
 
-    private static StringBuilder index;
+    private StringBuilder index;
 
-    public static void init() throws IOException {
+    public Git() throws IOException {
+        init();
+    }
+
+    public void init() throws IOException {
         File objectsFolder = new File("objects");
         if (!objectsFolder.exists())
             objectsFolder.mkdirs();
@@ -26,7 +33,7 @@ public class Git {
         index = new StringBuilder("");
     }
 
-    public static void blob(String filename) throws IOException, NoSuchAlgorithmException {
+    public void blob(String filename) throws IOException, NoSuchAlgorithmException {
         BufferedReader br = new BufferedReader(new FileReader(filename));
         StringBuilder fileContent = new StringBuilder();
 
@@ -45,19 +52,19 @@ public class Git {
         br.close();
     }
 
-    public static String generateSha1(String fileContent) throws NoSuchAlgorithmException {
+    public String generateSha1(String fileContent) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         byte[] ret = (md.digest(fileContent.getBytes()));
         return byteArrayToHexString(ret);
     }
 
-    public static void addToIndex() throws IOException {
+    public void addToIndex() throws IOException {
         PrintWriter pw = new PrintWriter("index");
         pw.print(index.toString().substring(0, index.length() - 1));
         pw.close();
     }
 
-    public static String byteArrayToHexString(byte[] b) {
+    public String byteArrayToHexString(byte[] b) {
         String result = "";
         for (int i = 0; i < b.length; i++) {
             result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
@@ -65,7 +72,7 @@ public class Git {
         return result;
     }
 
-    public static void remove(String filename) throws IOException {
+    public void remove(String filename) throws IOException {
         BufferedReader brIndex = new BufferedReader(new FileReader("index"));
         StringBuilder index2 = new StringBuilder();
         boolean removedItem = false;
@@ -94,6 +101,73 @@ public class Git {
 
         brIndex.close();
     }
+
+    public String addDirectory(String folderName) throws Exception {
+        File directory = new File(folderName);
+        File[] fileList = directory.listFiles();
+        Tree tree = new Tree();
+        StringBuilder sb = new StringBuilder();
+
+        for(File f : fileList) {
+            if(f.isDirectory()) {
+                addDirectory(folderName + "/" + f.getName());
+                Tree tree2 = new Tree();
+                File[] list = f.listFiles();
+                for(File file : list) {
+                    tree2.add("blob : " + generateHash(file) + " : " + file.getName());
+                }
+                sb.append("tree : " + tree2.writeToObjects() + '\n');
+                tree.add(sb.toString());
+
+            }
+            else {
+                String name = f.getName();
+                blob(folderName + "/" + name);
+                BufferedReader br = new BufferedReader(new FileReader(folderName + "/" + name));
+                while(br.ready()) {
+                    sb.append((char) br.read());
+                }
+                String contents = sb.toString();
+                String sha1 = generateSha1(contents);
+                StringBuilder sb2 = new StringBuilder();
+                if(sb2.length() != 0)
+                    sb2.append('\n');
+                sb2.append("blob : " + sha1 + " : " + name);
+                tree.add(sb2.toString());
+            }
+        }
+        tree.writeToObjects();
+        removeNewLine("objects/" + tree.writeToObjects());
+        return(tree.writeToObjects());
+    }
+
+    public String generateHash(File f) throws IOException, NoSuchAlgorithmException {
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        StringBuilder sb = new StringBuilder();
+        while(br.ready()) {
+            sb.append((char) br.read());
+        }
+        String contents = sb.toString();
+        return generateSha1(contents);
+    }
+
+    public void removeNewLine(String filename) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+
+        while(br.ready()) {
+            char c = (char) br.read();
+            if(br.ready())
+                sb.append(c);
+            else
+                break;
+        }
+
+        FileWriter fw = new FileWriter(filename);
+        fw.write(sb.toString());
+        fw.close();
+    }
+
 
     // Commented out the following methods because they aren't necessary for Index
 
--- GitTester.java
+++ GitTester.java
@@ -11,13 +11,15 @@ public class GitTester {
     @Test
     @DisplayName("Verify sha1 hashing works")
     void testGenerateSha1() throws Exception {
-        assertEquals(Git.generateSha1("Hello world."), "e44f3364019d18a151cab7072b5a40bb5b3e274f");
+        Git git = new Git();
+        assertEquals(git.generateSha1("Hello world."), "e44f3364019d18a151cab7072b5a40bb5b3e274f");
     }
 
     @Test
     @DisplayName("Verify init creates index file and objects folder")
     void testInit() throws IOException {
-        Git.init();
+        Git git = new Git();
+        git.init();
 
         assertTrue(Util.exists("objects"));
         assertTrue(Util.exists("index"));
@@ -31,11 +33,12 @@ public class GitTester {
     void testBlob() throws Exception {
         // Because blob is designed to create the blob and update the index, this test
         // tests both of those functionalities
+        Git git = new Git();
 
-        Git.init();
+        git.init();
 
         Util.writeFile("testFile.txt", "This is a test file.");
-        Git.blob("testFile.txt");
+        git.blob("testFile.txt");
 
         // Confirm blob file has been created in objects with the correct hash
         assertTrue(Util.exists("objects/26d82f1931cbdbd83c2a6871b2cecd5cbcc8c26b"));
@@ -53,19 +56,21 @@ public class GitTester {
     @Test
     @DisplayName("Verify remove correctly updates index")
     void testRemove() throws Exception {
-        Git.init();
+        Git git = new Git();
+
+        git.init();
 
         Util.writeFile("testFile.txt", "This is a test file.");
         Util.writeFile("testFile2.txt", "This is another test file.");
 
-        Git.blob("testFile.txt");
-        Git.blob("testFile2.txt");
+        git.blob("testFile.txt");
+        git.blob("testFile2.txt");
 
         // Confirm index has both files
         assertEquals(Util.readFile("index"),
                 "testFile.txt : 26d82f1931cbdbd83c2a6871b2cecd5cbcc8c26b\ntestFile2.txt : dd6fdaba4cff3db9692d2a86b39a331ad92c0667");
 
-        Git.remove("testFile.txt");
+        git.remove("testFile.txt");
 
         // Confirm testFile.txt has been removed from index
         assertEquals(Util.readFile("index"), "testFile2.txt : dd6fdaba4cff3db9692d2a86b39a331ad92c0667");
@@ -73,7 +78,7 @@ public class GitTester {
         // Confirm the blob file still exists
         assertTrue(Util.exists("objects/26d82f1931cbdbd83c2a6871b2cecd5cbcc8c26b"));
 
-        Git.remove("testFile2.txt");
+        git.remove("testFile2.txt");
 
         // Confirm testFile2.txt has been removed from index
         assertEquals(Util.readFile("index"), "");
